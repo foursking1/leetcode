1e24631500e647709a8894d5b5bb3b523a651966
foursking <foursking@gmail.com>
2015-02-25 11:32:41 +0800

    add some file

---
+++ LRU-Cache.py
@@ -0,0 +1,19 @@
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = collections.OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        val = self.cache[key]
+        del self.cache[key]
+        self.cache[key] = val
+        return val
+
+    def set(self, key, value):
+        if key in self.cache:
+            del self.cache[key]
+        elif len(self.cache) == self.capacity:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
